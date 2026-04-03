6abf79074ebda4f00936b6d45a363153a94142e6
kali <kali@kali.kali>
2026-04-03 08:57:06 -0400

    final final db fix

--- run.py
+++ run.py
@@ -1,14 +1,21 @@
 import os
 from dotenv import load_dotenv
-from app import create_app
+from app import create_app, db
 
 load_dotenv()
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
+# ✅ VERY IMPORTANT (models load karne ke liye)
+from app.models import user, config
+
+# ✅ DB create
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
     app.run(
-        host=os.getenv('HOST', '127.0.0.1'),
+        host='0.0.0.0',
         port=int(os.getenv('PORT', 5000)),
         debug=app.config.get('DEBUG', False)
     )
